665add1bbf70b6322cb605b39ee1352947fe6720
Marko1212 <shihonage7@gmail.com>
2021-04-28 07:48:03 +0200

    small fix

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -34,7 +34,7 @@ return (
 
             {
                 posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm={6}>
+                    <Grid key={post._id} item xs={12} sm={6} md={6}>
                         <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
 
