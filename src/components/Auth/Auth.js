f40334f309bd3d6c7a73e70e76e1047562ed857a
Marko1212 <shihonage7@gmail.com>
2021-05-03 15:17:17 +0200

    authentification suite

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
@@ -6,14 +6,21 @@ import Input from './Input';
 
 const Auth = () => {
     const classes = useStyles();
+    const {showPassword, setShowPassword} = useState(false); 
 
     const isSignup = false;
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
     const handleSubmit = () => {
 
     };
 
     const handleChange = () => {
+
+    }
+
+    const switchMode = () => {
 
     }
 
@@ -31,10 +38,24 @@ const Auth = () => {
                             isSignup && (
                                 <>
                                         <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                        <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                                        <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                                 </>
                             )
                         }
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                        <Input name="password" label="Pasword" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                   
+                        { isSignup &&  <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="flex=end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"}
+                            </Button>
+                        </Grid>
                     </Grid>
                 </form>
             </Paper>
