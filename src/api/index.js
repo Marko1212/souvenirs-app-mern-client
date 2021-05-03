0d52037f4e5fe7137a20f7fc3bdfa8a068218dea
Marko1212 <shihonage7@gmail.com>
2021-05-03 13:25:50 +0200

    start authentification

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 
-//const url = "http://localhost:5000/posts";
+const url = "http://localhost:5000/posts";
 
-const url = 'https://souvenirs-app-mern.herokuapp.com/posts';
+//const url = 'https://souvenirs-app-mern.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
