b4aab691acab9161e85d0c631e50e3a612e62580
Marko1212 <shihonage7@gmail.com>
2021-05-07 07:40:41 +0200

    authentication continued

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,24 +4,36 @@ import { GoogleLogin } from 'react-google-login';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Icon from "./icon";
 import useStyles from './styles';
 import Input from './Input';
-import Icon from "./icon"
+import {signin, signup} from '../../actions/auth';
+
+
+const initialState = {firstName: '', lastName: '', email: '', password: '', confirmPassword: ''};
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleShowPassword = () => setShowPassword(prevShowPassword => !prevShowPassword);
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+           
+        if (isSignup) {
+            dispatch(signup(formData, history));
+        } else {
+            dispatch(signin(formData, history));
+        }
     };
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
 
     };
 
