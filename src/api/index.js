ef7c01843d18ee870b9227da59f1c889c327132e
Marko1212 <shihonage7@gmail.com>
2021-05-08 07:55:02 +0200

    update

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,13 @@ const API = axios.create({baseURL: "http://localhost:5000"});
 
 // const API = axios.create({baseURL: "https://souvenirs-app-mern.herokuapp.com"});
 
+API.interceptors.request.use((req) => {
+if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+}
+return req;
+});
+
 
 export const fetchPosts = () => API.get('/posts');
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,7 +11,7 @@ const Form = ({ currentId, setCurrentId }) => {
    const inputRef = useRef(null);
 
     const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
+        title: '', message: '', tags: '', selectedFile: ''
     });
 
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
@@ -19,6 +19,8 @@ const Form = ({ currentId, setCurrentId }) => {
     const classes = useStyles();
 
     const dispatch = useDispatch();
+
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
         if (post) setPostData(post);
@@ -28,22 +30,32 @@ const Form = ({ currentId, setCurrentId }) => {
         e.preventDefault();
 
         if (currentId) {
-            dispatch(updatePost(currentId, postData));
+            dispatch(updatePost(currentId, {...postData, name : user?.result?.name }));
         } else {
-            dispatch(createPost(postData));
+            dispatch(createPost({...postData, name : user?.result?.name }));
         }
 
         clear();
+    };
+
+    if (!user?.result?.name) {
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Please Sign In to create your own memories and like other's memories.
+                </Typography>
+            </Paper>
+        );
     }
 
-    //TODO : Clear the file input on clear or submit
+    //TODO : Clear the FILE input on clear or submit
     
     const clear = () => {
 
 
         setCurrentId(0);
         setPostData({
-            creator: '', title: '', message: '', tags: '', selectedFile: ''
+            title: '', message: '', tags: '', selectedFile: ''
         });
 
         inputRef.current.value = '';
@@ -54,7 +66,6 @@ const Form = ({ currentId, setCurrentId }) => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">{currentId ? "Modifier" : "Créer"} un Souvenir</Typography>
-                <TextField name="creator" variant="outlined" label="Créateur" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Titre" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
                 <TextField name="tags" variant="outlined" label="Tags (séparés par des virgules)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
